194190c42a9edf3858108e4f3854f40460d8eaa8
Leah <leahgwin@gmail.com>
2018-09-27 11:23:27 -0500

    added Dapper and corrected Models Namespace

--- WorkforceManagement/WorkforceManagement/Controllers/EmployeeController.cs
+++ WorkforceManagement/WorkforceManagement/Controllers/EmployeeController.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using WorkforceManagement.Models;
+using System.Data.SqlClient;
 
 namespace WorkforceManagement.Controllers
 {
     public class EmployeeController : Controller
     {
+        private readonly IConfiguration _config;
+
+        public EmployeeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+
+
         // GET: Employee
         public ActionResult Index()
         {
