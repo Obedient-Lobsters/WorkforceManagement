0fda71662b2be1eacc16534a50e3a535457738ee
Ab101200 Ab101200 <millerac333@gmail.com>
2018-09-27 11:57:47 -0500

    added to Employee Model and created a Razor View File for employee details

--- WorkforceManagement/WorkforceManagement/Models/Employee.cs
+++ WorkforceManagement/WorkforceManagement/Models/Employee.cs
@@ -12,9 +12,11 @@ namespace WorkforceManagement.Models
         public int EmployeeId { get; set; }
 
         [Required]
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
 
         [Required]
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
 
         [Required]
@@ -26,8 +28,21 @@ namespace WorkforceManagement.Models
         [Required]
         public int DepartmentId { get; set; }
 
+        [Display(Name = "Department")]
         public Department Department { get; set; }
 
+        [Display(Name = "Assigned Computer")]
         public Computer Computer { get; set; }
+
+        [Display(Name = "Employee Name")]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+        [Display(Name = "Assigned Training Programs")]
+        public List<TrainingProgram> TrainingPrograms { get; set; } = new List<TrainingProgram>();
     }
 }
