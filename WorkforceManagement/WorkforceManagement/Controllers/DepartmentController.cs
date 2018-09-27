fe402d6d1289c5a4f11d25d0adbbd2b450b30ace
Shu52 <shusajid@gmail.com>
2018-09-27 15:03:25 -0500

    Added more comments

--- WorkforceManagement/WorkforceManagement/Controllers/DepartmentController.cs
+++ WorkforceManagement/WorkforceManagement/Controllers/DepartmentController.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
-using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Configuration;
 using WorkforceManagement.Models;
 using System.Data.SqlClient;
