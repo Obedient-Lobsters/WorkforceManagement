4965f9861b4ee6eee390059be9317c23a15d0ca5
Ab101200 Ab101200 <millerac333@gmail.com>
2018-09-27 15:56:04 -0500

    currently working on EMPLOYEE DETIAL sql query in EmployeeController

--- WorkforceManagement/WorkforceManagement/Controllers/EmployeeController.cs
+++ WorkforceManagement/WorkforceManagement/Controllers/EmployeeController.cs
@@ -30,85 +30,128 @@ namespace WorkforceManagement.Controllers
             }
         }
 
-        // GET: Employee
-        public ActionResult Index()
+        public async Task<IActionResult> Details(int? id)
         {
-            return View();
-        }
-
-        // GET: Employee/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Employee/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Employee/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
+            if (id == null)
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
+                return NotFound();
             }
-            catch
-            {
-                return View();
-            }
-        }
+            //detail query NOT FINISHED
+            string sql = $@"
+            SELECT
+                e.EmployeeId,
+                e.FirstName,
+                e.LastName,
+                e.DepartmentId,
+				d.DepartmentId,
+				d.DepartmentName,
+				ec.EmployeeComputerId,
+				c.ComputerId,
+				c.Manufacturer,
+				c.ModelName
+				
+            FROM Employee e
+            JOIN Department d 
+			ON e.DepartmentId= d.DepartmentId
+			JOIN 
+			ON
+            WHERE e.EmployeeId = EmployeeId"
 
-        // GET: Employee/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Employee/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
+            using (IDbConnection conn = Connection)
             {
-                // TODO: Add update logic here
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
+                Employee employee
+                    = (await conn.QueryAsync<Employee>(sql)).ToList().Single();
+
+                if (employee == null)
+                {
+                    return NotFound();
+                }
+
+                return View(employee);
             }
         }
 
-        // GET: Employee/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
 
-        // POST: Employee/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
+        //// GET: Employee
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// GET: Employee/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: Employee/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Employee/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: Employee/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: Employee/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: Employee/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: Employee/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+    }
     }
 }
